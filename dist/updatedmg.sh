00a61684081d0e95f4e8b245386dfafac761d2f6
Anthony Orapallo <anthony_orapallo@cable.comcast.com>
2015-07-20 15:34:06 -0400

    new dmg and directory structre

---
+++ dist/updatedmg.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+if [ "$#" -ne 1 ]; then
+  echo "Usage: $0 <dmg file>"
+  echo "Example: $0 testapp.dmg"
+  exit 1
+fi
+#name="${1%.*}"
+#echo "$name"
+updatefile=osx/Info.plist
+dmg=osx/xre2/XRE2.dmg
+size=`ls -l "$1" | cut -d" " -f7`
+sline=16
+srep="      <string>$size<\/string>"
+echo "$srep"
+# using : instead of / due slashes in replacement text
+sed -i "" -e "${sline}s:.*:${srep}:" $updatefile
+hash=`openssl sha1 -binary "$1" | openssl base64`
+hline=14
+hrep="      <string>$hash<\/string>"
+echo "$hrep"
+sed -i "" -e "${hline}s:.*:${hrep}:" $updatefile
+cp $1 pxscene/dist/osx/xre2/XRE2.dmg
+cd pxscene
+git commit -a
+git push
+cd ..
