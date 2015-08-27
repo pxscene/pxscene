aebb5967ba7ae416b9a274d1cc89c5d7cd08a70f
Anthony Orapallo <anthony_orapallo@cable.comcast.com>
2015-08-27 14:41:18 -0400

    manually merged updatedmg

--- dist/osx/updatedmg.sh
+++ dist/osx/updatedmg.sh
@@ -5,27 +5,8 @@ if [ "$#" -ne 1 ]; then
   printf "Example: $0 deploy/MacOSX/pxscene.dmg\n"
   exit 1
 fi
-<<<<<<< HEAD
-#name="${1%.*}"
-#echo "$name"
-updatefile=osx/Info.plist
-dmg=osx/pxscene/pxscene.dmg
-size=`ls -l "$1" | cut -d" " -f7`
-sline=16
-srep="      <string>$size<\/string>"
-echo "$srep"
-# using : instead of / due slashes in replacement text
-sed -i "" -e "${sline}s:.*:${srep}:" $updatefile
-hash=`openssl sha1 -binary "$1" | openssl base64`
-hline=14
-hrep="      <string>$hash<\/string>"
-echo "$hrep"
-sed -i "" -e "${hline}s:.*:${hrep}:" $updatefile
-cp $1 osx/pxscene/pxscene.dmg
-git status
-=======
 
-FILE=osx/Info.plist
+FILE=Info.plist
 SIZE=`ls -l "$1" | cut -d" " -f7`
 SLINE=16
 SREP="      <string>${SIZE}<\/string>"
@@ -43,8 +24,3 @@ sed -i "" -e "${HLINE}s:.*:${HREP}:" ${FILE}
 printf "Copying ${1} to ${DEST}\n"
 cp ${1} ${DEST}
 printf "Update complete.\n"
-#git status
->>>>>>> a5c128e209b0ed6fce59e531457131226019430a
-#git commit -a
-#git push
-#cd ..
