fdbf75cb20de47c31589a975d690c996c41399a1
tcarro004 <Thomas_Carroll2@cable.comcast.com>
2015-09-20 21:52:31 -0400

    Update to latest pxCore...rcvrcore changes which now use the import of px:scene1.js to get Scene.  nativeScene is now abstracted out.

--- examples/px-reference/tutorials/helloworld.js
+++ examples/px-reference/tutorials/helloworld.js
@@ -1,8 +1,15 @@
 // scene is provided to the module when it is created
-scene.create({
-  t:"text",                // Element type will be text
-  parent: scene.root,      // Parent element
-  text:"Hello, World!",    // the text
-  x:100, y:100,            // position
-  textColor:0xff0000ff,    // RGBA - red text
-  pixelSize:64});          // font height
+px.import("px:scene.1.js").then( function ready(scene) {
+
+  scene.create({
+    t: "text",                // Element type will be text
+    parent: scene.root,       // Parent element
+    text: "Hello, World!",    // the text
+    x: 100, y: 100,           // position
+    textColor:0xff0000ff,     // RGBA - red text
+    pixelSize:64              // font height
+  });
+
+}).catch(function importFailed(err){
+    console.log("Import failed for helloworld.js: " + err);
+});
