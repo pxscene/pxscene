365b19be4f859865e289fb149c9393a91c57f370
Jason Coelho <jason_coelho@comcast.com>
2016-08-26 12:17:20 -0400

    updated components example - replaced scaling with explicity w and h and stretch x,y values

--- examples/px-reference/gallery/components1.js
+++ examples/px-reference/gallery/components1.js
@@ -9,81 +9,81 @@
 //
 // Jason Coelho
 
-var remote    = "https://diatrophy.github.io/pxComponents/";
+var remote = "https://diatrophy.github.io/pxComponents/";
 var localhost = "http://localhost:8090/";
 
-px.configImport({"components:":remote});
+px.configImport({ "components:": localhost });
 
 px.import({
-    scene           : "px:scene.1.js",
-    uiMath          : 'components:math.js',
-    uiImageRenderer : 'components:image/imageRenderer.js',
-    uiImageEffects  : 'components:image/imageEffects.js',
-    uiImage         : 'components:image/image.js'}).then( function importsAreReady(imports) {
+  scene: "px:scene.1.js",
+  uiMath: 'components:math.js',
+  uiImageRenderer: 'components:image/imageRenderer.js',
+  uiImageEffects: 'components:image/imageEffects.js',
+  uiImage: 'components:image/image.js'
+}).then(function importsAreReady(imports) {
 
-    var scene           = imports.scene,
-        uiImage         = imports.uiImage,
-        uiImageRenderer = imports.uiImageRenderer(scene),
-        uiImageEffects  = imports.uiImageEffects,
-        randomInt       = imports.uiMath().randomInt,
-        root      = scene.root;
+  var scene = imports.scene,
+    uiImage = imports.uiImage,
+    uiImageRenderer = imports.uiImageRenderer(scene),
+    uiImageEffects = imports.uiImageEffects,
+    randomInt = imports.uiMath().randomInt,
+    root = scene.root;
 
-    var basePackageUri = px.getPackageBaseFilePath();
+  var basePackageUri = px.getPackageBaseFilePath();
 
-    // TODO - ideally some of these images may be generic enough that they can eventually live in the framework rather
-    // than in the examples
-    var bgUrl = basePackageUri+"/images/cork.png",
-        bgDropShadowUrl = basePackageUri+"/images/radial_gradient.png",
-        shadowUrl = basePackageUri+"/images/BlurRect.png";
+  // TODO - ideally some of these images may be generic enough that they can eventually live in the framework rather
+  // than in the examples
+  var bgUrl = basePackageUri + "/images/cork.png",
+    bgDropShadowUrl = basePackageUri + "/images/radial_gradient.png",
+    shadowUrl = basePackageUri + "/images/BlurRect.png";
 
-    // first create the background cork board image
-    var bgImage = uiImageRenderer.render(
-                    uiImage({url:bgUrl,parent:root,stretchX:2,stretchY:2,w:scene.w,h:scene.h})
-                      .addEffects(uiImageEffects()
-                        // .topShadow(bgDropShadowUrl)
-                        ), function(bgImage){
+  // first create the background cork board image
+  var bgImage = uiImageRenderer.render(
+    uiImage({ url: bgUrl, parent: root, stretchX: 2, stretchY: 2, w: scene.w, h: scene.h })
+      .addEffects(uiImageEffects().topShadow(bgDropShadowUrl)), function (bgImage) {
 
-        var photoUrl = basePackageUri+"/images/photos/"+ "IMG_4765.jpg";
+        var photoUrl = basePackageUri + "/images/photos/" + "IMG_4765.jpg",
+          polaroidWidth = 300
 
-        // image rendered with polaroid of 300px width, drop shadow 10px and top shadow
-        var p1 = uiImageRenderer.render(uiImage({url:photoUrl,parent:root,x:50,y:50,sx:0.10,sy:0.10})
-                    .addEffects(uiImageEffects()
-                      .polaroid(300)
-                      .topShadow(bgDropShadowUrl)
-                      .dropShadow(shadowUrl,10)
-                    ));
+        // image rendered with polaroid, drop shadow and top shadow
+        uiImageRenderer.render(uiImage({ url: photoUrl, parent: root, x: 50, y: 50 })
+          .addEffects(uiImageEffects()
+            .polaroid(polaroidWidth)
+            .topShadow(bgDropShadowUrl)
+            .dropShadow(shadowUrl)
+          ));
 
-        // image rendered with polaroid width 250px and shadow 10px
-        var p2 = uiImageRenderer.render(uiImage({url:photoUrl,parent:root,x:450,y:50,sx:0.10,sy:0.10})
-                    .addEffects(uiImageEffects()
-                      .polaroid(250)
-                      .dropShadow(shadowUrl,10)
-                    ));
+        // image rendered with polaroid and shadow
+        uiImageRenderer.render(uiImage({ url: photoUrl, parent: root, x: 450, y: 50 })
+          .addEffects(uiImageEffects()
+            .polaroid(polaroidWidth)
+            .dropShadow(shadowUrl)
+          ));
 
-        // image rendered with polaroid effect with width 400px
-        var p3 = uiImageRenderer.render(uiImage({url:photoUrl,parent:root,x:850,y:50,sx:0.10,sy:0.10})
-                    .addEffects(uiImageEffects()
-                      .polaroid(400)
-                    ));
+        // image rendered with polaroid effect
+        uiImageRenderer.render(uiImage({ url: photoUrl, parent: root, x: 850, y: 50 })
+          .addEffects(uiImageEffects()
+            .polaroid(polaroidWidth)
+          ));
 
         // image rendered with no effects
-        var p4 = uiImageRenderer.render(uiImage({url:photoUrl,parent:root,x:50,y:580,sx:0.10,sy:0.10})
-                    .addEffects(uiImageEffects()
-                    ));
+        uiImageRenderer.render(uiImage({ url: photoUrl, parent: root, x: 50, y: 480, w: 300, h: 300, stretchX: 1, stretchY: 1 })
+          .addEffects(uiImageEffects()
+          ));
 
         // image rendered with top shadow
-        var p5 = uiImageRenderer.render(uiImage({url:photoUrl,parent:root,x:450,y:580,sx:0.10,sy:0.10})
-                    .addEffects(uiImageEffects()
-                      .topShadow(bgDropShadowUrl)
-                    ));
+        uiImageRenderer.render(uiImage({ url: photoUrl, parent: root, x: 450, y: 480, w: 300, h: 300, stretchX: 1, stretchY: 1 })
+          .addEffects(uiImageEffects()
+            .topShadow(bgDropShadowUrl)
+          ));
 
-        // image rendered with drop shadow of 10px
-        var p6 = uiImageRenderer.render(uiImage({url:photoUrl,parent:root,x:850,y:580,sx:0.10,sy:0.10})
-                    .addEffects(uiImageEffects()
-                      .dropShadow(shadowUrl,10)
-                    ));
-    });
+        // image rendered with drop shadow
+        uiImageRenderer.render(uiImage({ url: photoUrl, parent: root, x: 850, y: 480, w: 300, h: 300, stretchX: 1, stretchY: 1 })
+          .addEffects(uiImageEffects()
+            .dropShadow(shadowUrl)
+          ));
+      });
 
-}).catch( function(err){
-    console.error("Error: " + err);
+}).catch(function (err) {
+  console.error(err.stack);
 });
