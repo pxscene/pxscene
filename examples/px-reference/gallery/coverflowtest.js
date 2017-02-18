c9970034dce8f414333d28a77dab4018f3a85f2c
cfry <constance_fry@cable.comcast.com>
2017-02-18 08:55:06 -0800

    remove expired samples

--- examples/px-reference/gallery/coverflowtest.js
+++
@@ -1,7 +0,0 @@
-px.import("px:scene.1.js").then( function ready(scene) {
-  var root = scene.root;
- 
-  var s = scene.create({t:"scene",parent:root,w:1200,h:400,url:"http://www.pxscene.org/examples/px-reference/gallery/coverflow.js"});
-  s.focus = true;
-
-});
