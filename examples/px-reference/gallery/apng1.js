5d9ccf2951ff275bc08a011465fafb2616152aec
John Robinson <johnrobinsn@gmail.com>
2017-02-21 11:18:52 -0500

    added a plain png and a jpeg

--- examples/px-reference/gallery/apng1.js
+++ examples/px-reference/gallery/apng1.js
@@ -3,19 +3,28 @@ px.import("px:scene.1.js").then( function ready(scene) {
   let basePackageUri = px.getPackageBaseFilePath();
 
   let urls = [
-  "http://www.pxscene.org/examples/px-reference/gallery/images/apng/elephant.png",
-  "http://www.pxscene.org/examples/px-reference/gallery/images/apng/cube.png",  
-  "http://www.pxscene.org/examples/px-reference/gallery/images/apng/spinfox.png",
+  "http://www.pxscene.org/examples/px-reference/gallery/images/apng/elephant.png", // apng
+  "http://www.pxscene.org/examples/px-reference/gallery/images/apng/cube.png",     // apng
+  "http://www.pxscene.org/examples/px-reference/gallery/images/apng/spinfox.png",  // apng
+  "http://www.pxscene.org/examples/px-reference/gallery/images/star.png",          // supports plain old pngs
+  "http://www.pxscene.org/examples/px-reference/gallery/images/ajpeg.jpg",       // and single frame jpegs too!!
   ];
 
   let ready = urls.map(url=>{return scene.create({t:"imageA",url:url,parent:scene.root}).ready.catch(e=>{return null})})
 
   Promise.all(ready).then(values=>{
-    let x = 0;
+    let x = 0; let y = 0; let rowHeight = 0;
     for (let o of values) {
       if (!o) continue;
-      o.animateTo({x:x},1.5,scene.animation.TWEEN_STOP);
-      x += o.w; 
+      if (x+o.w > scene.getWidth())
+      {
+        x = 0;
+        y += rowHeight;
+        rowHeight = 0;
+      }
+      o.animateTo({x:x,y:y},1.5,scene.animation.TWEEN_STOP);
+      x += o.w;
+      rowHeight = Math.max(rowHeight,o.h);
     }
   });
 
