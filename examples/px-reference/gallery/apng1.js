ba0d791f7a20890dec939e7f6fa8fd8e9d3885f7
John Robinson <johnrobinsn@gmail.com>
2017-02-21 10:30:33 -0500

    Improved error handling

--- examples/px-reference/gallery/apng1.js
+++ examples/px-reference/gallery/apng1.js
@@ -1,18 +1,19 @@
 px.import("px:scene.1.js").then( function ready(scene) {
 
-  var basePackageUri = px.getPackageBaseFilePath();
+  let basePackageUri = px.getPackageBaseFilePath();
 
-  var urls = [
+  let urls = [
   "http://www.pxscene.org/examples/px-reference/gallery/images/apng/elephant.png",
-  "http://www.pxscene.org/examples/px-reference/gallery/images/apng/cube.png",
+  "http://www.pxscene.org/examples/px-reference/gallery/images/apng/cube.png",  
   "http://www.pxscene.org/examples/px-reference/gallery/images/apng/spinfox.png",
   ];
 
-  var ready = urls.map(url=>{return scene.create({t:"imageA",x:0,url:url,parent:scene.root}).ready;})
+  let ready = urls.map(url=>{return scene.create({t:"imageA",url:url,parent:scene.root}).ready.catch(e=>{return null})})
 
   Promise.all(ready).then(values=>{
-    var x = 0;
-    for (var o of values) {
+    let x = 0;
+    for (let o of values) {
+      if (!o) continue;
       o.animateTo({x:x},1.5,scene.animation.TWEEN_STOP);
       x += o.w; 
     }
