d6286385b3b8c1ffb37be3affa6611a406bb6c2e
John Robinson <johnrobinsn@gmail.com>
2017-02-24 15:41:43 -0500

    added layout when resizing window

--- examples/px-reference/gallery/apng1.js
+++ examples/px-reference/gallery/apng1.js
@@ -7,14 +7,16 @@ px.import("px:scene.1.js").then( function ready(scene) {
   "http://www.pxscene.org/examples/px-reference/gallery/images/apng/cube.png",     // apng
   "http://www.pxscene.org/examples/px-reference/gallery/images/apng/spinfox.png",  // apng
   "http://www.pxscene.org/examples/px-reference/gallery/images/star.png",          // supports plain old pngs
-  "http://www.pxscene.org/examples/px-reference/gallery/images/ajpeg.jpg",       // and single frame jpegs too!!
+  "http://www.pxscene.org/examples/px-reference/gallery/images/ajpeg.jpg",         // and single frame jpegs too!!
   ];
 
   let ready = urls.map(url=>{return scene.create({t:"imageA",url:url,parent:scene.root}).ready.catch(e=>{return null})})
 
-  Promise.all(ready).then(values=>{
+  let images = {}
+
+  let layout = ()=>{
     let x = 0; let y = 0; let rowHeight = 0;
-    for (let o of values) {
+    for (let o of images) {
       if (!o) continue;
       if (x+o.w > scene.getWidth())
       {
@@ -22,12 +24,19 @@ px.import("px:scene.1.js").then( function ready(scene) {
         y += rowHeight;
         rowHeight = 0;
       }
-      o.animateTo({x:x,y:y},1.5,scene.animation.TWEEN_STOP);
+      o.animateTo({x:x,y:y},1,scene.animation.TWEEN_STOP);
       x += o.w;
       rowHeight = Math.max(rowHeight,o.h);
-    }
+    }  
+  };
+
+  Promise.all(ready).then(values=>{
+    images = values;
+    layout();
   });
 
+  scene.on("onResize", e=>{layout();});
+  
 }).catch( function importFailed(err){
   console.error("Import failed for fancy.js: " + err)
 });
