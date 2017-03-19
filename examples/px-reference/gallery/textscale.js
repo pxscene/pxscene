20ca4e807337d4d9fe7ce301ac4ee7c6b99e494d
John Robinson <johnrobinsn@gmail.com>
2017-03-19 12:12:27 -0400

    Cleanup and a few comments

--- examples/px-reference/gallery/textscale.js
+++ examples/px-reference/gallery/textscale.js
@@ -1,12 +1,22 @@
 px.import("px:scene.1.js").then( function ready($) {
 
-    var t1 = $.create({t:"text",text:"Iñtërnâtiônàližætiøn",parent:$.root,x:0,y:0,pixelSize:24,a:0.75,sx:1,sy:1});  // white
-    t1.animateTo({sx:20,sy:20},5,$.animation.TWEEN_LINEAR,$.animation.OPTION_OSCILLATE,$.animation.COUNT_FOREVER);
-    var t2 = $.create({t:"text",text:"Iñtërnâtiônàližætiøn",parent:$.root,x:0,y:0,pixelSize:120,textColor:0xff0000ff}); // red
-    t2.animateTo({sx:0.1,sy:0.1},5,$.animation.TWEEN_LINEAR,$.animation.OPTION_OSCILLATE,$.animation.COUNT_FOREVER);
-    var t3 = $.create({t:"text",text:"Iñtërnâtiônàližætiøn",parent:$.root,x:0,y:0,pixelSize:24,textColor:0xffff00ff});  // yellow
-    t3.animateTo({sx:20.0,sy:50},5,$.animation.TWEEN_LINEAR,$.animation.OPTION_OSCILLATE,$.animation.COUNT_FOREVER);
-    var t4 = $.create({t:"text",text:"Iñtërnâtiônàližætiøn",parent:$.root,x:0,y:0,pixelSize:24,textColor:0x00ffffff});  // cyan
-    t4.animateTo({sx:50,sy:20.0},5,$.animation.TWEEN_LINEAR,$.animation.OPTION_OSCILLATE,$.animation.COUNT_FOREVER);
+    let a = 0.75;
+    let small = 24;
+    let large = 240;
+    let red = 0xff0000ff;
+    let white = 0xffffffff;
+    let cyan = 0x00ffffff;
+    let yellow = 0xffff00ff;
+    let str = "Iñtërnâtiônàližætiøn";
 
+    // small pixelSize scale up and down
+    $.create({t:"text",text:str,parent:$.root,pixelSize:small,a:1.0,textColor:white})
+        .animateTo({sx:20,sy:20},5,$.animation.TWEEN_LINEAR,$.animation.OPTION_OSCILLATE,$.animation.COUNT_FOREVER);
+    $.create({t:"text",text:str,parent:$.root,pixelSize:small,a:a,textColor:cyan})
+        .animateTo({sx:3.0,sy:20},5,$.animation.TWEEN_LINEAR,$.animation.OPTION_OSCILLATE,$.animation.COUNT_FOREVER);
+    $.create({t:"text",text:str,parent:$.root,pixelSize:small,a:a,textColor:yellow})
+        .animateTo({sx:20,sy:3.0},5,$.animation.TWEEN_LINEAR,$.animation.OPTION_OSCILLATE,$.animation.COUNT_FOREVER);
+    // large pixelSize scale down and up
+    $.create({t:"text",text:str,parent:$.root,pixelSize:large,a:a,textColor:red}) 
+        .animateTo({sx:0.1,sy:0.1},5,$.animation.TWEEN_LINEAR,$.animation.OPTION_OSCILLATE,$.animation.COUNT_FOREVER);
 });
