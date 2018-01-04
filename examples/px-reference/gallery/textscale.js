1e67adb281f5b0cc48bb0be69c34c8840eed94c0
John Robinson <johnrobinson@Johns-MacBook-Pro.local>
2018-01-04 16:22:32 -0500

    changed textscale.js to be duktape compatible... and renamed original to  a separate es2015 variant

--- examples/px-reference/gallery/textscale.js
+++ examples/px-reference/gallery/textscale.js
@@ -1,23 +1,21 @@
-px.import("px:scene.1.js").then( function ready($) {
+"use strict";
 
-    let a = 0.75;
-    let small = 24;
-    let large = 240;
-    let red = 0xff0000ff;
-    let white = 0xffffffff;
-    let cyan = 0x00ffffff;
-    let yellow = 0xffff00ff;
-    let black = 0x000000ff;
-    let str = "Iñtërnâtiônàližætiøn";
+px.import("px:scene.1.js").then(function ready($) {
+
+    var a = 0.75;
+    var small = 24;
+    var large = 240;
+    var red = 0xff0000ff;
+    var white = 0xffffffff;
+    var cyan = 0x00ffffff;
+    var yellow = 0xffff00ff;
+    var black = 0x000000ff;
+    var str = "Iñtërnâtiônàližætiøn";
 
     // small pixelSize scale up and down
-    $.create({t:"text",text:str,parent:$.root,pixelSize:small,a:1.0,textColor:red})
-        .animateTo({sx:20,sy:20},5,$.animation.TWEEN_LINEAR,$.animation.OPTION_OSCILLATE,$.animation.COUNT_FOREVER);
-    $.create({t:"text",text:str,parent:$.root,pixelSize:small,a:a,textColor:white})
-        .animateTo({sx:3.0,sy:20},5,$.animation.TWEEN_LINEAR,$.animation.OPTION_OSCILLATE,$.animation.COUNT_FOREVER);
-    $.create({t:"text",text:str,parent:$.root,pixelSize:small,a:a,textColor:yellow})
-        .animateTo({sx:20,sy:3.0},5,$.animation.TWEEN_LINEAR,$.animation.OPTION_OSCILLATE,$.animation.COUNT_FOREVER);
+    $.create({ t: "text", text: str, parent: $.root, pixelSize: small, a: 1.0, textColor: red }).animateTo({ sx: 20, sy: 20 }, 5, $.animation.TWEEN_LINEAR, $.animation.OPTION_OSCILLATE, $.animation.COUNT_FOREVER);
+    $.create({ t: "text", text: str, parent: $.root, pixelSize: small, a: a, textColor: white }).animateTo({ sx: 3.0, sy: 20 }, 5, $.animation.TWEEN_LINEAR, $.animation.OPTION_OSCILLATE, $.animation.COUNT_FOREVER);
+    $.create({ t: "text", text: str, parent: $.root, pixelSize: small, a: a, textColor: yellow }).animateTo({ sx: 20, sy: 3.0 }, 5, $.animation.TWEEN_LINEAR, $.animation.OPTION_OSCILLATE, $.animation.COUNT_FOREVER);
     // large pixelSize scale down and up
-    $.create({t:"text",text:str,parent:$.root,pixelSize:large,a:a,textColor:cyan}) 
-        .animateTo({sx:0.1,sy:0.1},5,$.animation.TWEEN_LINEAR,$.animation.OPTION_OSCILLATE,$.animation.COUNT_FOREVER);
+    $.create({ t: "text", text: str, parent: $.root, pixelSize: large, a: a, textColor: cyan }).animateTo({ sx: 0.1, sy: 0.1 }, 5, $.animation.TWEEN_LINEAR, $.animation.OPTION_OSCILLATE, $.animation.COUNT_FOREVER);
 });
