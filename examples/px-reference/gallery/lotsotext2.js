3a49243061211fe34a3c3882bb59b70bdb9626a6
conniefry <constance_fry@cable.comcast.com>
2018-04-12 17:05:29 -0700

    simplify example

--- examples/px-reference/gallery/lotsotext2.js
+++ examples/px-reference/gallery/lotsotext2.js
@@ -9,18 +9,12 @@ px.import("px:scene.1").then(function(scene) {
     scene.create({t:'fontResource',url:'http://www.pxscene.org/examples/px-reference/fonts/DancingScript-Regular.ttf'})
   ]
 
-  function randomFont() {
-    var val = Math.random();
-    if( val < 0.3) return 0;
-    if(val >0.3 && val < 0.6) return 1;
-    return 2;
-  }
   function randomColor() {
     return (Math.random()*(1<<24)<<8)|0xff
   }
-
+  var fontNum = 0;
   for (var i = 0; i < 2500; i++) {
-    scene.create({t:"text",text:"Spark Rocks!",parent:root,pixelSize:40,textColor:randomColor(),font:fonts[randomFont()]}).ready.then(function(o){
+    scene.create({t:"text",text:"Spark Rocks!",parent:root,pixelSize:40,textColor:randomColor(),font:fonts[fontNum++]}).ready.then(function(o){
       o.x = (Math.random()*(scene.w-o.w-(margin*2)))+margin;
       o.y = (Math.random()*(scene.h-o.h-(margin*2)))+margin;
       o.a = (Math.random()*0.7)+0.3;
@@ -31,6 +25,7 @@ px.import("px:scene.1").then(function(scene) {
       var speed = (Math.random()*5)+0.3;
       o.animateTo({r:o.r+(360*direction)},speed,scene.animation.LINEAR,scene.animation.OPTION_LOOP, scene.animation.COUNT_FOREVER);
     });
+    if(fontNum >2) fontNum = 0;
   }
   
 });
